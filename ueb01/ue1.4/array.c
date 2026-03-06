62243d32c17c26b65d6cf672056a774c3d746e35
Fafo <schmickl.fabio@gmail.com>
2026-03-06 16:53:38 +0100

    Refactor Fibonacci-Programm: Integriere die printArray-Funktion in die main-Funktion und optimiere die Verwendung von Array-Größe

--- ueb01/ue1.4/array.c
+++ ueb01/ue1.4/array.c
@@ -3,26 +3,33 @@
 #include <stdint.h>
 #include <time.h>
 
+void printArray(const uint8_t *arr, int size, int start) {
+    if (arr == NULL || start < 0 || start >= size)
+        return;
+
+    for (int i = start; i < size; i++) {
+        printf("F[%d] = %u\n", i, arr[i]);
+    }
+}
+
 int main(void) {
-    uint8_t fib[8];
+    int size = 8;
+    uint8_t fib[size];
     int index;
 
     fib[0] = 0;
     fib[1] = 1;
 
-    for (int i = 2; i < 8; i++) {
+    for (int i = 2; i < size; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
     }
 
-    srand(time(NULL)); 
-    index = rand() % 8; // Zufälliger Index zwischen 0 und 7
+    srand(time(NULL));
+    index = rand() % size; // Zufälliger Index zwischen 0 und size-1
 
     printf("Zufaelliger Index: %d\n\n", index);
 
-    // Ausgabe der Fibonacci-Zahlen ab dem zufälligen Index
-    for (int i = index; i < 8; i++) {
-        printf("F[%d] = %u\n", i, fib[i]);
-    }
+    printArray(fib, size, index);
 
     return 0;
 }
